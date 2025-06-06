c9c1db3fbdcec4d48eb9d5244f639e238a74dba5
gostok <stiv4ik12012@gmail.com>
2025-06-06 17:50:37 +0300

    update 0.0.4 | menu page

--- backend/parm_and_jam_django/parm_and_jam_django/urls.py
+++ backend/parm_and_jam_django/parm_and_jam_django/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_app.urls')),
+    path('menu/', include('menu_app.urls')),
 ]
